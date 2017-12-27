833fc2f9f595c977997e855a27682c1e699b693b
Isheeta Shah <isheeta_s@yahoo.com>
2017-12-26 16:44:22 -0800

    functioning table for expenses and add expenses with date parsing

--- angularapp.js
+++ angularapp.js
@@ -5,16 +5,29 @@ app.controller('MainCtrl', [
 '$scope',
 function($scope){
 	$scope.expenses = [
-		//{date: '12/17/2017 02:30 AM', category: 'Gas', price: 29.01, location: 'Shell'}
 	];
 	$scope.addExpense = function(){
+		let price = "0.00";
+		let mm = 00;
+		let dd = 00;
+		let yyyy = 0000;
+		if($scope.price.indexOf('.') == -1)
+			price = '$'+$scope.price + '.00';
+		else
+			price = '$'+$scope.price;
+
+		mm = $scope.date.getMonth()+1;
+		dd = $scope.date.getDate()+1;
+		yyyy = $scope.date.getFullYear();
+
+
 		$scope.expenses.push({
-			date: $scope.date,
+			date: mm+'/'+dd+'/'+yyyy,
 			category: $scope.category,
-			price: $scope.price,
+			price: price,
 			location: $scope.location
 		});
-		$scope.date = '01/01/2000';
+		$scope.date = '';
 		$scope.category = '';
 		$scope.price = 0.00;
 		$scope.location = '';
