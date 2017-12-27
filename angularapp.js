a54171d321f90e937fb7ee44d8025bca26a3be3a
Isheeta Shah <isheeta_s@yahoo.com>
2017-12-27 10:18:03 -0800

    Added comments. Fixed dd of date to not add 1.

--- angularapp.js
+++ angularapp.js
@@ -4,20 +4,24 @@ var app = angular.module('budgetSystem', []);
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
+	//this should hold the items retrieved from the database and display
 	$scope.expenses = [
 	];
+	//this should also add the item to the database
 	$scope.addExpense = function(){
 		let price = "0.00";
 		let mm = 00;
 		let dd = 00;
 		let yyyy = 0000;
+		if($scope.price == null)
+			return;
 		if($scope.price.indexOf('.') == -1)
 			price = '$'+$scope.price + '.00';
 		else
 			price = '$'+$scope.price;
 
 		mm = $scope.date.getMonth()+1;
-		dd = $scope.date.getDate()+1;
+		dd = $scope.date.getDate();
 		yyyy = $scope.date.getFullYear();
 
 
